ae872abe4cba2abc8d1ba79558520e5e7b74a699
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-21 17:38:55 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,11 +1,6 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-  def up 
-  end 
   
-  def down 
-  end
-  
-  def move
+  def change
     create_table :songs do |a|
       a.string :name
       a.integer :artist_id
