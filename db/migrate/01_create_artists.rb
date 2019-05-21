ee56eacb193d5003ca95e88e96af378ff562a4f6
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-21 17:41:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,5 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
   def change
-    create_table artists {|a| a.string :name}
+    create_table artists do |a| 
+      a.string :name
+    end
   end
 end
